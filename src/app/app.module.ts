f88f99e6caa41e93d4b07d538ee714e79f72a125
puliwang@cxist.com <puliwang@cxist.com>
2021-09-04 17:12:36 +0800

    添加菜单路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,31 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { HomeComponent } from './components/home/home.component';
+
+registerLocaleData(zh);
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        NzMenuModule
     ],
-    providers: [],
+    providers: [{ provide: NZ_I18N, useValue: zh_CN }],
     bootstrap: [AppComponent]
 })
 export class AppModule { }

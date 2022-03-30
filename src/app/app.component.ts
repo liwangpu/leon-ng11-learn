673e11fbabc018805f7425fed9d25b6909da029f
leon <puliwang@cxist.com>
2022-03-30 20:39:15 +0800

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import * as Excel from "exceljs/dist/exceljs.min.js";
+
+//  let workbook = new Excel.Workbook();
 
 @Component({
     selector: 'app-root',
@@ -8,11 +12,57 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-    public constructor() {
-
+    public form: FormGroup;
+    public constructor(
+        private fb: FormBuilder
+    ) {
+        this.form = this.fb.group({
+            files: []
+        });
     }
 
     public ngOnInit(): void {
+
+    }
+
+    // public onChange(fs: FileList): void {
+    //     const files = [];
+    //     for (let idx = fs.length - 1; idx >= 0; idx--) {
+    //         files.push(fs[idx]);
+    //     }
+    //     this.form.patchValue({ files });
+    //     console.log('value:', this.form.value)
+    // }
+
+    parseExcelFile2(files:FileList) {
+        if (!files.length) return;
+        var file = files[0];
+
+        console.time();
+        var reader = new FileReader();
+        reader.onloadend = function (event) {
+            var arrayBuffer = reader.result;
+            // var buffer = Buffer.from(arrayBuffer)
+            // debugger
+
+            var workbook = new Excel.Workbook();
+            // workbook.xlsx.read(buffer)
+            workbook.xlsx.load(arrayBuffer).then(function (workbook) {
+                // console.timeEnd();
+                var result = ''
+                workbook.worksheets.forEach(function (sheet) {
+                    sheet.eachRow(function (row, rowNumber) {
+                        result += row.values + ' | \n'
+                    })
+                })
+                // result2.innerHTML = result
+                console.log('result:',result);
+            });
+        };
+        reader.readAsArrayBuffer(file);
+    }
+
+    public async readExcel(): Promise<void> {
 
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
     declarations: [
@@ -11,7 +13,9 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
